97df9efbebf491fb34b0ccec0925731b1bc7480d
Jenifer <jnf.asm@gmail.com>
2023-06-01 17:39:41 +0100

    Testar o componente NotFound.js

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { getByRole, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+import App from '../App';
+
+describe('Ao favoritar a partir da página de detalhes', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const noFavorite = screen.getByText('No favorite Pokémon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  test('Teste se apenas são exibidos os Pokémon favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // pega o elemento na Home clica em 'More Details' clica no checkbox
+    act(() => {
+      history.push('/pokemon/25');
+    });
+
+    const favoritePokemon = getByRole('checkbox');
+
+    // simula a acao do user
+    userEvent.click(favoritePokemon);
+
+    // faz o teste
+    expect(favoritePokemon).toBeChecked();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/semnome');
+    });
+
+    const notFoundHeading = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(notFoundHeading).toBeInTheDocument();
+    expect(notFoundHeading).toHaveTextContent('Page requested not found');
+  });
+
+  test('Teste se a página mostra a imagem pré-definida', () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/semnome');
+    });
+
+    const notFoundImage = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundImage).toBeInTheDocument();
+  });
+});
